8e635d949640dc132f04e544686e6420d8622317
MustafaBioS <bioshpere.voices@gmail.com>
2025-09-29 23:25:49 +0300

    done

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ login_manager.login_view = 'login'
 
 class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key= True)
-    username = db.Column(db.String(64), nullable=False)
+    username = db.Column(db.String(64), nullable=False, unique=True)
     password = db.Column(db.Text, nullable=False)
 
 # ROUTES
